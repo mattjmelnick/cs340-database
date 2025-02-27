4007366e85f17e0364484a57165fa8505c5f7f9e
Matthew Melnick <melnicma@classwork.engr.oregonstate.edu>
2025-02-27 11:57:29 -0800

    Place db_connection in each route to prevent timeout errors

--- blueprints/customers.py
+++ blueprints/customers.py
@@ -2,20 +2,24 @@ from flask import Blueprint, render_template, request, redirect, url_for
 import database.db_connector as db
 
 customers_bp = Blueprint('customers', __name__)
-db_connection = db.connect_to_database()
 
 @customers_bp.route('/customers', methods=["GET", "POST"])
 def customers():
+    db_connection = db.connect_to_database()
     if request.method == "POST":
+        # get the input data from the add customer form
         name = request.form.get("customerName")
         email = request.form.get("customerEmail")
         phone = request.form.get("customerPhone")
+        # add the new customer to the database
         query = f"INSERT INTO Customers (name, email, phone_number) VALUES ('{name}', '{email}', '{phone}')"
         db.execute_query(db_connection=db_connection, query=query)
 
+	    # return to customers page
         return redirect(url_for('customers.customers'))
 
     if request.method == "GET":
+        # display the data from the database in the table
         query = "SELECT * FROM Customers"
         cursor = db.execute_query(db_connection=db_connection, query=query)
         results = cursor.fetchall()
@@ -24,18 +28,25 @@ def customers():
 
 @customers_bp.route('/edit_customer/<int:customer_id>', methods=["POST"])
 def edit_customer(customer_id):
+    db_connection = db.connect_to_database()
+    # get the input data from the update customer form
     name = request.form.get("customerNameUpdate")
     email = request.form.get("customerEmailUpdate")
     phone = request.form.get("customerPhoneUpdate")
+    # update the customer in the database using the input data
     query = f"UPDATE Customers SET name = '{name}', email = '{email}',\
         phone_number = '{phone}' WHERE customer_id = {customer_id}"
     db.execute_query(db_connection=db_connection, query=query)
 
+	# return to customers page
     return redirect(url_for('customers.customers'))
 
 @customers_bp.route('/delete_customer/<int:customer_id>', methods=["POST"])
 def delete_customer(customer_id):
+    db_connection = db.connect_to_database()
+    # delete the selected customer from the database
     query = f"DELETE FROM Customers WHERE customer_id = {customer_id}"
     db.execute_query(db_connection=db_connection, query=query)
 
+	# return to customers page
     return redirect(url_for('customers.customers'))
--- blueprints/raffle_orders.py
+++ blueprints/raffle_orders.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template, request, redirect, url_for
 import database.db_connector as db
 
 raffle_orders_bp = Blueprint('raffle_orders', __name__)
-db_connection = db.connect_to_database()
 
 @raffle_orders_bp.route('/raffle_orders', methods=["GET", "POST"])
 def raffle_orders():
+    db_connection = db.connect_to_database()
     if request.method == "POST":
         order_id = request.form.get("roOrderID")
         raffle_id = request.form.get("roRaffleID")
@@ -16,7 +16,7 @@ def raffle_orders():
             query = f"INSERT INTO RaffleOrders (order_id, raffle_id) VALUES ({order_id}, {raffle_id})"
         db.execute_query(db_connection=db_connection, query=query)
 
-		# return to the raffle orders page
+        # return to raffle orders page
         return redirect(url_for('raffle_orders.raffle_orders'))
 
     if request.method == "GET":
@@ -24,12 +24,12 @@ def raffle_orders():
         cursor = db.execute_query(db_connection=db_connection, query=query)
         raffle_order_data = list(cursor.fetchall())
 
-		# get the list of order ids from the Orders table
+        # get the list of order ids from the Orders table
         order_query = "SELECT order_id FROM Orders"
         cursor = db.execute_query(db_connection=db_connection, query=order_query)
         order_data = list(cursor.fetchall())
 
-		# get the list of raffle ids from the Raffles table
+        # get the list of raffle ids from the Raffles table
         raffle_query = "SELECT raffle_id FROM Raffles"
         cursor = db.execute_query(db_connection=db_connection, query=raffle_query)
         raffle_data = list(cursor.fetchall())
@@ -39,6 +39,7 @@ def raffle_orders():
 
 @raffle_orders_bp.route('/edit_raffle_order/<int:raffle_order_id>', methods=["POST"])
 def edit_raffle_order(raffle_order_id):
+    db_connection = db.connect_to_database()
     order_id = request.form.get("roOrderIDUpdate")
     raffle_id = request.form.get("roRaffleIDUpdate")
     # check for selected null value
@@ -48,14 +49,16 @@ def edit_raffle_order(raffle_order_id):
     else:
         query = f"UPDATE RaffleOrders SET order_id = {order_id}, raffle_id = {raffle_id}\
             WHERE raffle_order_id = {raffle_order_id}"
-    
     db.execute_query(db_connection=db_connection, query=query)
     
+    # return to raffle orders page
     return redirect(url_for('raffle_orders.raffle_orders'))
 
 @raffle_orders_bp.route('/delete_raffle_order/<int:raffle_order_id>', methods=["POST"])
 def delete_raffle_order(raffle_order_id):
+    db_connection = db.connect_to_database()
     query = f"DELETE FROM RaffleOrders WHERE raffle_order_id = {raffle_order_id}"
     db.execute_query(db_connection=db_connection, query=query)
 
+    # return to raffle orders page
     return redirect(url_for('raffle_orders.raffle_orders'))
